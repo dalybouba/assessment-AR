b65bf55d45a85184c26b6515177ae446148971af
dalybouba <simorghconsulting90@gmail.com>
2021-01-16 17:52:59 +0100

    display selected challenge

---
+++ src/app/selected-item/selected-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ChallengesService } from '../services/challenges.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-selected-item',
+  templateUrl: './selected-item.component.html',
+  styleUrls: ['./selected-item.component.css']
+})
+export class SelectedItemComponent implements OnInit {
+  challenges$: Observable<any>;
+  challenge: any = {};
+  id: number;
+  constructor(private activatedRouter: ActivatedRoute, private challengesServices: ChallengesService) { }
+
+  ngOnInit(): void {
+    this.id = + this.activatedRouter.snapshot.paramMap.get('id');
+    this.getItemById();
+  }
+
+  getItemById() {
+    this.challengesServices.getChallenges().subscribe(
+      data => {
+        this.challenge = data[this.id]
+      }
+    );
+  }
+}
